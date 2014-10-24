7beedc7b6e07459b856ecf4fda2aaba3ec3d25f3
Sean Herman <seanherman@gmail.com>
2014-10-24 16:50:44 -0400

    updated gitignore to ignore PyCharm

--- framework/problem2.py
+++ framework/problem2.py
@@ -2,11 +2,13 @@ import decision_tree
 import scan
 import utils
 
+FILE_NAME = ('/Users/sean/Sync/cornell/modern-analytics-fall-2014/'
+  'hw2-bucket/data/finefoods.txt')
 
 def main():
     binary_label = True
 
-    data = scan.scan('foods.txt', binary_label)
+    data = scan.scan(FILE_NAME, binary_label)
     length = len(data)
 
     train_data = data[:int(length*.8)]
