3262e8383ef9739a52b22075b35639fe36e80d34
Bryan Russett <brbryanr@gmail.com>
2014-10-28 22:34:45 -0400

    update B, add C

--- program1/B.py
+++ program1/B.py
@@ -33,13 +33,12 @@ def distance(point1,point2,length):
     return math.sqrt(distance)
 
 def knn(trndata,tstdata,k):
-    nbrs = NearestNeighbors(n_neighbors=1, algorithm='brute').fit(trndata[:2000,1:])
+    nbrs = NearestNeighbors(n_neighbors=k, algorithm='brute').fit(trndata[:2000,1:])
     distances,indices = nbrs.kneighbors(tstdata[:1000,1:])
     print "Root Mean Squared Error: ", round(sqrt(mean_squared_error(tstdata[indices,0], trndata[indices,0])),1)
     coef,pval = stats.pearsonr(tstdata[indices,0], trndata[indices,0])
     print "Correlation Coefficient: ", round(coef[0],4)
-    print "Means Absolute Error: ", round(np.mean(np.sum(np.absolute(tstdata[indices,0] - trndata[indices,0]))),1)
-
+    print "Mean Absolute Error: ", round(np.mean(np.absolute(tstdata[indices,0] - trndata[indices,0])),1)
 def main():
     trndata = load_data(config.TRAIN_DATA)
     tstdata = load_data(config.TEST_DATA)
